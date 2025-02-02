5228aae08ad3105e0643373ca27b219b9f31c2c6
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2025-02-02 00:01:27 +0000

    ready for grading

--- ocean-levels/main.cpp
+++ ocean-levels/main.cpp
@@ -1,8 +1,31 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 2/1/25
 // Ocean Levels Programming Project
 // COSC 1030
 //
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    #//Variables
+    float years;
+    float oceanRisingPerYear = 1.5;
+
+    //calculations for 5 years
+    years = 5;
+    cout << "After " << years << " years, the ocean will have rised " << years * oceanRisingPerYear << " millimeters." << endl;
+    
+    //calculations for 7 years
+    years = 7;
+    cout << "After " << years << " years, the ocean will have rised " << years * oceanRisingPerYear << " millimeters." << endl;
+
+    //calculations for 10 years
+    years = 10;
+    cout << "After " << years << " years, the ocean will have rised " << years * oceanRisingPerYear << " millimeters." << endl;
+
+    return 0;
+}
 
 
