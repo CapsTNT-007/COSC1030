4fec7a369fefa5d5dbfd55a637cecb52491c8562
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2025-02-10 00:59:58 +0000

    Ready for grading

--- stadium-seating/main.cpp
+++ stadium-seating/main.cpp
@@ -1,7 +1,29 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 2/9/24
 // Stadium Seating Programming Project
 // COSC 1030
-//
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+int main(){
+
+    double seatsA;
+    double seatsB;
+    double seatsC;
+
+    cout << "How many people bought tickets in section A, B, and C. Put a space between all of them, then click enter. ";
+    cin >> seatsA >> seatsB >> seatsC;
+    seatsA *= 15;
+    seatsB *= 12;
+    seatsC *= 9;
+    cout << fixed << setprecision(2);
+    cout << "Income for section A: $" << seatsA << endl;
+    cout << "Income for section B: $" << seatsB << endl;
+    cout << "Income for section C: $" << seatsC << endl;
+
+
+    return 0;
+}
--- woody-furniture-company/main.cpp
+++ woody-furniture-company/main.cpp
@@ -1,7 +1,31 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 2/9/25
 // Woody Furniture Company Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+int main(){
+
+    double americanColonial;
+    double modern;
+    double frenchClassical;
+
+    cout << "How many chairs of each type were sold? Put a space between all of them, then click enter. ";
+    cin >> americanColonial >> modern >> frenchClassical;
+    americanColonial *= 85;
+    modern *= 57.5;
+    frenchClassical *= 127.75;
+    cout << fixed << setprecision(2);
+    cout << "Total cost of all American Colonial chairs sold: $" << americanColonial << endl;
+    cout << "Total cost of all Modern chairs sold: $" << modern << endl;
+    cout << "Total cost of all French Classical chairs sold: $" << frenchClassical << endl;
+    cout << "Total cost of all chairs: $" << americanColonial + modern + frenchClassical << endl;
+
+
+    return 0;
+}
