adaf4e8f16c172d1952aad61cfcb9eb2ffbe3d9f
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2025-02-24 00:03:07 +0000

    ready for grading

--- beverages-survey/main.cpp
+++ beverages-survey/main.cpp
@@ -1,7 +1,47 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 2/23/2025
 // Beverages Survey Programming Project
 // COSC 1030
 //
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //Variables
+    int count = 0;
+    int coffee = 0;
+    int tea = 0;
+    int coke = 0;
+    int orangeJuice = 0;
+
+    //Initializing loop
+    while (count != -1) {
+        cout << "If your favorite drint is coffee, please type 1" << endl;
+        cout << "If your favorite drint is tea, please type 2" << endl;
+        cout << "If your favorite drint is coke, please type 3" << endl;
+        cout << "If your favorite drint is oragne juice, please type 4" << endl;
+        cout << "If there is no one else in line, pleyse type -1" << endl;
+        cout << "Then press Enter" << endl;
+
+        //Getting value
+        cin >> count;
+
+        //Assesing the count variable and adding it to the total number of likes
+        if (count == 1)
+            coffee++;
+        else if (count ==2)
+            tea++;
+        else if (count ==3)
+            coke++;
+        else if (count ==4)
+            orangeJuice++;
+        else if (count != -1)
+            cout << "That's not one of the specified values. Please try again and input a number between 1 and 4, or -1 if there is no one else." << endl;
+    }
+    //Outputing the answer
+    cout << "There was " << coffee + tea + coke + orangeJuice << " people surveyed, with " << coffee << " people who liked coffee, " << tea << " people who liked tea, " << coke << " people who like coke, and " << orangeJuice << " people who liked orange Juice." << endl; 
+    return 0;
+}
 
--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,25 @@
 //
-// Name
-// Date
+// Name Tayson Weheeler
+// Date 2/23/2025
 // Calories Burned Programming Project
 // COSC 1030
 //
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    //Variables
+    int minutes = 5;
+
+    //Initializing loop
+    while (minutes <= 30) {
+
+        //Calculating and outputing answer
+        cout << "After " << minutes << " minutes, you will have burned " << minutes * 3.6 << " calories." << ' '<<endl;
+
+        //setting up for next loop
+        minutes += 5;
+    }
+    return 0;
+}
