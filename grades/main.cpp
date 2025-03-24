92d6de13cae2f23e7fd8fb6d3f32a80a70e03be0
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2025-03-24 04:22:16 +0000

    Ready for grading

--- grades/main.cpp
+++ grades/main.cpp
@@ -1,7 +1,52 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 3/13/25
 // Grades Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+int findingAverage(double, double);
+
+int main(){
+    //Variables
+    int grades;
+    double numberOfGrades;
+    double sumOfGrades;
+    int average;
+    int count = 0;
+
+    //Getting information
+    cout << "How many grades do you need to imput?" << endl;
+    cin >> numberOfGrades;
+
+    while (count != numberOfGrades){
+        cout << "What is the grade?" << endl;
+        cin >> grades;
+        sumOfGrades += grades;
+        count++;
+    }
+
+    //Running function
+    average = findingAverage (sumOfGrades, numberOfGrades);
+
+    //Determining grade
+    if (average >= 90)
+        cout << "The average grade is an A" << endl;
+    else if (average >= 80)
+        cout << "The average grade is an B" << endl;
+    else if (average >= 70)
+        cout << "The average grade is an c" << endl;
+    else if (average >= 60)
+        cout << "The average grade is an D" << endl;
+    else
+        cout << "The average grade is an F" << endl;
+    return 0;
+}
+
+//Defining function
+int findingAverage(double total, double amout){
+    return total/amout;
+}
--- markup/main.cpp
+++ markup/main.cpp
@@ -1,7 +1,33 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 3/23/25
 // Markup Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+void calculateRetail(double, double);
+
+int main(){
+    //Variables
+    double wholesale;
+    double markup;
+
+    //Getting information
+    cout << "What is the wholesale price of the item?" << endl;
+    cin >> wholesale;
+    cout << "What is the markup percent of the item?" << endl;
+    cin >> markup;
+
+    //Running function
+    calculateRetail(wholesale, markup);
+
+    return 0;
+}
+
+//Defining function
+void calculateRetail(double wholesale, double markup){
+    cout << "The retail price is $" << wholesale * (1 + markup / 100) << endl;
+}
