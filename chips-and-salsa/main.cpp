bc7ff8be5fb93ee2e252314de41df5ff9a3b6f71
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2025-03-31 05:36:44 +0000

    Ready for grading

--- chips-and-salsa/main.cpp
+++ chips-and-salsa/main.cpp
@@ -1,7 +1,54 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 3/30/2025
 // Chips and Salsa Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+int main(){
+    //Variables
+    const int salsas = 5;
+    string salsaTypes[salsas] = {"sweet", "sour", "spice", "hot", "zesty"};
+    int salsaSlales[salsas];
+    int totalSales = 0;
+    int lowestSalse = 0;
+    int highestSales = 0;
+
+    //Getting infromation
+    for (int count = 0; count < salsas; count++){
+        cout << "How many cans are you selling of " << salsaTypes[count] << "?" << endl;
+        cin >> salsaSlales[count];
+    }
+
+    //Calculating for total, lowest, and highest
+    for (int count = 0; count < salsas; count++){
+        totalSales += salsaSlales[count];
+    }
+
+    for (int count = 0; count < salsas; count++){
+        if (highestSales < salsaSlales[count]){
+            highestSales = salsaSlales[count];
+        }
+    }
+
+    lowestSalse = salsaSlales[0];
+    for (int count = 0; count < salsas; count++){
+        if (lowestSalse > salsaSlales[count]){
+            lowestSalse = salsaSlales[count];
+        }
+    }
+
+    //Presenting infromation
+    for (int count = 0; count < salsas; count++){
+        cout << "There was " << salsaSlales[count] << " jars sold of the " << salsaTypes[count] << " type of salsa." << endl;
+    }
+    cout << "The total was " << totalSales << endl;
+    cout << "The highest selling was " << highestSales << endl;
+    cout << "The lowest selling was " << lowestSalse << endl;
+
+    return 0;
+}
--- temperatures/main.cpp
+++ temperatures/main.cpp
@@ -1,7 +1,82 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 3/30/2025
 // Temperatures
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+//Setting up arrays and functions
+typedef int weather[50];
+double findingAverage(weather temps, int);
+int findingHighest(weather temps, int);
+int findingLowest(weather temps, int);
+
+int main() {
+
+    // Variables
+    weather temps;
+    const int salsas = 5;
+    int days;
+    double average = 0;
+    int highest = 0;
+    int lowest = 0;
+
+    //Getting infromation
+    cout << "How many days would you like to put temperatures in for? " << endl;
+    cin >> days;
+    while (days > 50) {
+        cout << "Please input 50 days or less. " << endl;
+        cin >> days;
+    }
+
+    for (int count = 0; count < days; count++) {
+        cout << "What was the temperature on day " << count + 1 << "?" << endl;
+        cin >> temps[count];
+    }
+
+    //Running fuctions
+    average = findingAverage(temps, days);
+    highest = findingHighest(temps, days);
+    lowest = findingLowest(temps, days);
+
+    //Outputing infromation
+    cout << fixed << setprecision(2);
+    cout << "The average temperature is " << average << endl;
+    cout << "The highest temperature is " << highest << endl;
+    cout << "The lowest temperature is " << lowest << endl;
+
+    return 0;
+}
+
+//Functions
+double findingAverage(weather temps, int days) {
+    double sum = 0;
+    for (int count = 0; count < days; count++) {
+        sum = sum + temps[count]; 
+    }
+    return sum / days;
+}
+
+int findingHighest(weather temps, int days) {
+    int highest = temps[0];
+    for (int count = 1; count < days; count++) {
+        if (highest < temps[count]) {
+            highest = temps[count];
+        }
+    }
+    return highest;
+}
+
+int findingLowest(weather temps, int days) {
+    int lowest = temps[0]; 
+    for (int count = 1; count < days; count++) { 
+        if (lowest > temps[count]) {
+            lowest = temps[count];
+        }
+    }
+    return lowest;
+}
