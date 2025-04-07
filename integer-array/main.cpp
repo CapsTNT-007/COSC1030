14d54a5d77f541c8077a843aa5800cb1f894546b
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2025-04-07 05:44:10 +0000

    ready for grading

--- integer-array/main.cpp
+++ integer-array/main.cpp
@@ -1,7 +1,119 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 4/6/2025
 // Integer Array
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+//Functions
+void bubbleSort(int [], int);
+void swap(int &, int &);
+int binarySearch(const int [], int, int);
+double mean(int [], int);
+
+int main(){
+    //Variables
+    int amountOfInt;
+    int value = 0;
+    int posistion = 0;
+    double meanFinal = 0;
+
+    //Getting size of array
+    cout << "How many values would you like to imput? " << endl;
+    cin >> amountOfInt;
+
+    //Making array
+    int ints[amountOfInt];
+
+    //Getting infromation in the array
+    for (int count = 0; count < amountOfInt; count++){
+        cout << "Enter integer: " << endl;
+        cin >> ints[count];
+    }
+
+    bubbleSort(ints, amountOfInt);
+
+    //Serching for a specific number
+    cout << "What value would you like to search for? " << endl;
+    cin >> value;
+
+    posistion = binarySearch(ints, amountOfInt, value);
+    cout << "The posistion of the number you are serching for is " << posistion << endl;
+    
+    //Getting mean
+    meanFinal = mean(ints, amountOfInt);
+    cout << "The mean of the array is " << meanFinal << endl;
+
+    return 0;
+}
+
+//Sorting the array
+void bubbleSort(int array[], int size)
+{
+    int maxElement;
+    int index;
+
+    for (maxElement = size -1; maxElement > 0; maxElement--)
+    {
+        for (index = 0; index < maxElement; index++)
+        {
+
+            if (array[index] > array [index + 1])
+            {
+                swap(array[index], array[index + 1]);
+            }
+        }
+    }
+}
+
+//Function for swaping two variables values
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+//Search function
+int binarySearch(const int array[], int numElems, int value)
+{
+    int first = 0,
+        last = numElems - 1,
+        middle,
+        position = -1;
+    bool found = false;
+
+    while (!found && first <= last)
+    {
+        middle = (first + last) / 2;
+        if (array[middle] == value)
+        {
+            found = true;
+            position = middle;
+        }
+        else if (array[middle] > value)
+            last = middle -1;
+        else
+            first = middle +1;
+    }
+    return position;
+}
+
+//Mean function
+double mean(int array[], int size)
+{
+    double total = 0;
+    double tally = 0;
+    double final = 0;
+    for (int count = 0; count < size; count++)
+    {
+        total += array[count];
+        tally++;
+    }
+    final = (double)total/tally;
+    return final;
+}
--- search-benchmarks/main.cpp
+++ search-benchmarks/main.cpp
@@ -1,8 +1,60 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 4/6/2025
 // Search Benchmarks Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    //Variables
+    const int SIZE = 20;
+    int integers[SIZE] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int value = 15;
+
+    int index = 0;
+    int position =-1;
+    bool found = false;
+
+    //Setting up the linear search
+    while (index < SIZE && !found)
+    {
+        if (integers[index] == value)
+        {
+            found = true;
+            cout << "It took " << index << " many comparisons." << endl;
+        }
+        index++;
+    }
+
+    //Resetting and adding new variables
+    int first = 0;
+    int last = SIZE - 1;
+    int middle =(first + last) / 2;
+    position = -1;
+    found = false;
+    int count = 1;
+
+    //Setting up binary search
+    while (!found && first <=last)
+    {
+        middle = (first + last) / 2;
+        if (integers[middle] == value)
+        {
+            found = true;
+            cout << "It took " << count << " many comparisons." << endl;
+        }
+        else if (integers[middle] > value)
+            last = middle - 1;
+        else
+            first = middle +1;
+        count++;
+    }
 
 
+    return 0;
+}
