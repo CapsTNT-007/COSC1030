1c4d4124113bd7d76df5c1466830034fa6230dab
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2025-04-14 04:53:24 +0000

    ready for grading

--- monthly-sales/main.cpp
+++ monthly-sales/main.cpp
@@ -1,7 +1,65 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 4/13/2025
 // Monthly Sales Programming Project
 // COSC 1030
 //
+#include <iostream>
 
+using namespace std;
+int yearly(int*, int);
+int average(int*, int);
+
+int main(){
+
+    //Variables
+    int *sales = nullptr;
+    int size;
+    int count;
+
+    //Getting input
+    cout << "How many values do you wish to imput?" << endl;
+    cin >> size;
+
+    //Creating Dynamic Memory Allocation Array
+    sales = new int[size]; 
+
+    for (count = 0; count < size; count++){
+        cout << "Enter value #" << count + 1 << ": ";
+        cin >> sales[count];
+    }
+
+    //Calling two functions
+    yearly(sales,size);
+    average(sales,size);
+
+    //Releasing dynamic memory allocation
+    delete[] sales; 
+
+    return 0;
+}
+
+//Functions
+int yearly(int *sales, int SIZE) {
+    int count;
+    int total = 0;
+
+    for (count = 0; count < SIZE; count++){
+        total += sales[count];
+    }
+    cout << "The yearly sum is: " << total << endl;
+
+    return 0;
+}
+
+int average(int *sales, int SIZE) {
+    int count;
+    double total = 0;
+
+    for (count = 0; count < SIZE; count++){
+        total += sales[count];
+    }
+    cout << "The average is: " << (double)total/(double)SIZE << endl;
+
+    return 0;
+}
--- pointer-rewrite/main.cpp
+++ pointer-rewrite/main.cpp
@@ -1,7 +1,34 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 4/13/2025
 // Pointer Rewrite Programming Project
 // COSC 1030
 //
+#include <iostream>
 
+using namespace std;
+int doSomething(int*, int*);
+
+int main(){
+
+    //Variables
+    int numb1 = 5;
+    int numb2 = 10;
+
+    //Getting info
+    cout << "number 1 = " << numb1 << ", number 2 = " << numb2 << endl;
+
+    int result = doSomething(&numb1,&numb2);
+
+    cout << "number 1 = " << numb1 << ", number 2 = " << numb2 << endl;
+
+    return 0;
+}
+
+//Functions
+int doSomething(int *x, int *y) {
+    int temp = *x;
+    *x = *y * 10;
+    *y = temp * 10;
+    return *x + *y;
+}
