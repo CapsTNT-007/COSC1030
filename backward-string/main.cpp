9fb4c92e585b90ee561833fa4bbb77ce03df6f84
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2025-04-21 05:05:07 +0000

    ready for grading

--- backward-string/main.cpp
+++ backward-string/main.cpp
@@ -1,7 +1,42 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 4/20/25
 // Backward String Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <cstring>
 
+using namespace std;
+
+int main(){
+
+    //Variables
+    const char size = 100;
+    char input[size];
+    char newLine[size];
+    int length;
+    int count;
+
+    //Input
+    cout << "Input a string: " << endl;
+    cin.getline(input, 100);
+
+    //Getting length of string
+    length = strlen(input);
+    int repeats = length;
+
+    //Reversing string
+    for (count = 0; count < repeats + 1; count++){
+        newLine[count] = input[length];
+        length--;
+    }
+
+    //Relaying new string
+    cout << "Your string backwards is: ";
+    for (count = 0; count < repeats + 1 ; count++){
+        cout << newLine[count];
+    }
+
+    return 0;
+}
--- pig-latin/main.cpp
+++ pig-latin/main.cpp
@@ -1,7 +1,63 @@
 //
-// Name
-// Date
+// Name Tayson Wheeler
+// Date 4/20/2025
 // Pig Latin Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <cstring>
 
+using namespace std;
+
+int main(){
+
+    //Variables
+    const char size = 100;
+    char input[size];
+    bool isWhite  = false;
+    char temp;
+    int count = 0;
+    int length;
+
+    //Input
+    cout << "Input a string: " << endl;
+    cin.getline(input, 100);
+
+    //Getting the length of input
+    length = strlen(input);
+
+    //Setting up a loop that will go through every character until the last one
+    while (count < length){
+
+        //Sets temp to the first letter of the word
+        temp = (input[count]);
+
+        //Looks through a word and moves the characters one spot back until it reaches a space (the end of the word)
+        while (isWhite == false && count < length){
+            if (isspace(input[count])){
+                isWhite = true;
+            } else {
+                input[count] = input[count + 1];
+                count++;
+            }
+        }
+
+        //Changes the final character in the word to the first and resets the loop
+        input[count - 1] = temp;
+        count++;
+        isWhite = false;
+
+    }
+
+    //Displays the end result and adds "ay" to the end of every word
+    cout << "Your string in pig latin is: ";
+    for (count = 0; count < length ; count++){
+        cout << input[count];
+        if (isspace(input[count+1]) || count + 1 == length){
+            cout << "ay";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
